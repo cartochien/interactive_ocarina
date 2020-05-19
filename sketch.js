9d8718400971085e75e930bc550ea19ad4c139cb
cartochien <chris.sourire@gmail.com>
2020-05-18 18:37:52 -0700

    Added delay to KeyReleased function and removed duplicate image issue

--- sketch.js
+++ sketch.js
@@ -49,12 +49,10 @@ function draw() {
     }
 
     if (keyrecorder.search("CADCAD") >= 0) { //Zelda's Lullaby
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[6].play(0.5);
         keyrecorder = "";
     } else if (keyrecorder.search("BDCBDC") >= 0) { //Saria's Song
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[7].play(0.2);
         effects[7].play(6.5); //Navi says "hey!"
@@ -62,7 +60,6 @@ function draw() {
         effects[9].play(8); //Saria laughs
         keyrecorder = "";
     } else if (keyrecorder.search("DBADBA") >= 0) { //Sun's Song
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[8].play();
         if (currentTime >= 6 && currentTime <= 18) {
@@ -72,7 +69,6 @@ function draw() {
         }
         keyrecorder = "";
     } else if (keyrecorder.search("EBAEBA") >= 0) { //Song of Storms 
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[9].play();
         effects[6].play(4.5); //thunder
@@ -80,14 +76,12 @@ function draw() {
         effects[6].play(6.2);
         keyrecorder = "";
     } else if (keyrecorder.search("DEBDEB") >= 0) { //Song of Time
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[10].play();
         effects[10].play(10);
         effects[11].play(15);
         keyrecorder = "";
     } else if (keyrecorder.search("ACDACD") >= 0) { //Epona's Song
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[11].play();
         effects[1].play(8.2); //Epona neighs
@@ -97,12 +91,10 @@ function draw() {
         effects[2].play(10.2);
         keyrecorder = "";
     } else if (keyrecorder.search("EACDCD") >= 0) { //Minuet of Forest 
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[0].play();
         keyrecorder = "";
     } else if (keyrecorder.search("BEBEDBDB") >= 0) { //Bolero of Fire AND AMPLITUDE
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[1].play();
         //fill(30,60,50);
@@ -110,22 +102,18 @@ function draw() {
         //rect(width/2, height/2, rectsize, rectsize);
         keyrecorder = "";
     } else if (keyrecorder.search("EBDDC") >= 0) { //Serenade of Water 
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[2].play();
         keyrecorder = "";
     } else if (keyrecorder.search("CDDECDB") >= 0) { //Nocturne of Shadow 
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[3].play();
         keyrecorder = "";
     } else if (keyrecorder.search("EBEDBE") >= 0) { //Requiem of Spirit 
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[4].play();
         keyrecorder = "";
     } else if (keyrecorder.search("ADADCA") >= 0) { //Prelude of Light
-        image(img[5], 0, 0, 420, 413.56);
         effects[0].play();
         songs[5].play();
         keyrecorder = "";
@@ -161,5 +149,5 @@ function keyPressed() {
 }
 
 function keyReleased() {
-    image(img[6], 0, 0, 420, 413.56);
+    image(img[6], 0, 0, 420, 413.56).delay(5);
 }
